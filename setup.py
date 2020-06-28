e55ee79b1fc73e389dc31c8e68c815da9dabaa65
Dmytro Vasylyshyn <dmytro_vasylyshyn@external.mckinsey.com>
2020-06-28 17:58:12 +0300

    Add six to install_requires [bugfix]

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     keywords=["sqlalchemy", "easy", "profile", "profiler", "profiling"],
-    install_requires=["sqlalchemy>=1.1,<1.4", "sqlparse>=0.3.0"],
+    install_requires=["sqlalchemy>=1.1,<1.4", "sqlparse>=0.3.0", "six"],
     tests_require=["coverage", "mock"],
     extras_require={"dev": ["tox", "bumpversion"]}
 )
